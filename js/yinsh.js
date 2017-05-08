da92a0384b939aec7c5fb23c5f0bbc2bd34e6860
Jakob Gruber <jakob.gruber@gmail.com>
2017-05-08 21:06:03 +0200

    Move validation for ring placement

--- js/yinsh.js
+++ js/yinsh.js
@@ -61,6 +61,40 @@ if (typeof module !== 'undefined' && exports) {
     }
   };
 
+  class Predicates {
+    static IsColor(expected) {
+      const predicates = [];
+      predicates[Yinsh.Color.Black] = IsBlack;
+      predicates[Yinsh.Color.White] = IsWhite;
+      return predicates[expected];
+    }
+    static IsBlack(color) { return color == Yinsh.Color.BLACK; }
+    static IsWhite(color) { return color == Yinsh.Color.WHITE; }
+    static IsRing(kind) { return kind == Yinsh.PieceKind.RING; }
+    static IsInactive(state) { return state == Yinsh.PieceState.INACTIVE; }
+    static IsActive(state) { return state == Yinsh.PieceState.ACTIVE; }
+  };
+
+  const Validators = [];
+
+  Validators[Yinsh.MoveKind.PLACE_RING] = function(move, state) {
+    if (state.current_player != move.player) return false;
+
+    const unused_player_rings = state.FilterPieces(
+      IsColor(move.player), IsRing, IsInactive);
+    if (unused_player_rings.length == 0) return false;
+
+    if (GetPieceAt(move.target_position) !== undefined) return false;
+
+    return true;
+  };
+
+  Yinsh.MoveValidator = class {
+    static Validate(move, state) {
+      return Validators[move.kind](move, state);
+    }
+  };
+
   Yinsh.GameState = class {
     constructor() {
       this.pieces = [];
@@ -85,6 +119,25 @@ if (typeof module !== 'undefined' && exports) {
       }
 
       return state;
+    }
+
+    FilterPieces(colorPredicate, kindPredicate, statePredicate) {
+      return this.pieces.filter(p => {
+        return colorPredicate(p.color) &&
+               kindPredicate(p.kind) &&
+               statePredicate(p.state);
+      });
+    }
+
+    GetPieceAt(position) {
+      const pieces = this.pieces.filter(p => {
+        if (IsInactive(p)) return false;
+        if (p.position == Yinsh.NoPosition) return false;
+        const {x, y, z} = p.position;
+        return position.x == x && position.y == y && position.z == z;
+      });
+      if (pieces.length != 1) return undefined;
+      return pieces[0];
     }
   };
 
